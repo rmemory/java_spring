0155b800cb80775cd864c8dac0e2febe8fb9001f
Richard Memory <richardfmemory@gmail.com>
2019-04-02 14:48:30 -0600

    Randomize strings from fortune service

--- spring-demo-one/src/com/springdemo/services/HappyFortuneService.java
+++ spring-demo-one/src/com/springdemo/services/HappyFortuneService.java
@@ -1,10 +1,18 @@
 package com.springdemo.services;
 
+import java.util.Random;
+
 public class HappyFortuneService implements IFortuneService {
+	private String[] fortuneArray = {
+			"This is your lucky day.",
+			"This is your unlucky day.",
+			"This is a could go either way day"
+	};
+	private Random rand = new Random();
 
 	@Override
 	public String getFortune() {
-		return "This is your lucky day.";
+		return fortuneArray[rand.nextInt(3)];
 	}
 
 }
