fd332822af8093284b91ae1378f936a453fb5aba
Richard Memory <richardfmemory@gmail.com>
2019-04-09 17:13:22 -0600

    Add initial maven spring demo

---
+++ spring_sample/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -0,0 +1,23 @@
+package com.pluralsight.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pluralsight.model.Customer;
+
+public class HibernateCustomerRepositoryImpl implements CustomerRepository {
+	/* (non-Javadoc)
+	 * @see com.pluralsight.repository.CustomerRepository#findAll()
+	 */
+	@Override
+	public List<Customer> findAll() {
+		List<Customer> customers = new ArrayList<Customer>();
+		
+		Customer customer = new Customer();
+		customer.setFirstName("Richard");
+		customer.setLastName("Memory");
+		customers.add(customer);
+		
+		return customers;
+	}
+}
